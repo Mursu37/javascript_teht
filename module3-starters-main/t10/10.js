526da8608b4849f8c0f7dbf7b85f613f2d542f90
Mursu <mursujesse@gmail.com>
2022-11-25 01:29:22 +0200

    Tehtävä 10 tehty.

--- module3-starters-main/t10/10.js
+++ module3-starters-main/t10/10.js
@@ -1,12 +1,15 @@
 
-const first = document.querySelector("input[name='firstname']")
-const last = document.querySelector("input[name='lastname']")
-const form = document.querySelector("form")
+const first = document.querySelector("input[name='firstname']");
+const last = document.querySelector("input[name='lastname']");
+const form = document.querySelector("form");
+const p = document.querySelector("p");
 console.log(first);
 console.log(last);
 console.log(form);
 
 function press(evt) {
     evt.preventDefault();
+    const name = `${first.value} ${last.value}`;
+    p.innerText = `Your name is ${name}`;
 }
-form.addEventListener("submit", press)
+form.addEventListener("submit", press);
